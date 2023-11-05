109e20d1d8e7324c43495175ef48af9e7bc4f733
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2023-11-04 21:11:17 -0500

    new

--- react-app-utd/src/App.js
+++ react-app-utd/src/App.js
@@ -13,11 +13,52 @@ function BasicButtonExample() {
   );
 }
 
+function CardContainer() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card">
+            <img src="your-image-url" className="card-img-top" alt="Image Alt Text" />
+            <div className="card-body">
+              <h5 className="card-title">Texas businesses, homeowners affected by tornado, severe storm can get help from SBA</h5>
+              <p className="card-text">Some text describing Card 1.</p>
+              <a href="#" className="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card">
+            <img src="your-image-url" className="card-img-top" alt="Image Alt Text" />
+            <div className="card-body">
+              <h5 className="card-title">Card 2</h5>
+              <p className="card-text">Some text describing Card 2.</p>
+              <a href="#" className="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card">
+            <img src="your-image-url" className="card-img-top" alt="Image Alt Text" />
+            <div className="card-body">
+              <h5 className="card-title">Card 3</h5>
+              <p className="card-text">Some text describing Card 3.</p>
+              <a href="#" className="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
       <div className='maincomponent'>
-
         <h1>InsuranceHelper</h1>
         <p>This web application will output the top natural disasters that happen in your state to help you get an idea of the insurance your small business may need</p>
         <BasicButtonExample/>
@@ -26,6 +67,7 @@ function App() {
         </div>
       </div>
       <div className="secondcomponent">
+        <CardContainer/>
         <h2 className='subheaders'>Customer Response Analysis</h2>
       </div>
     </div>
