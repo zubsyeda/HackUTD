3a99e9cdd87915e78a15f6820a0a3a53fef80d5e
choimyungbin <bbin852456.bbc@gmail.com>
2023-11-05 09:30:02 -0600

    cors added middleware

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(
+	cors({
+		origin: [
+			'https://localhost:3000',
+			'https://localhost:6000',
+			'https://localhost:6000',
+		],
+		methods: ['GET, POST, PUT, DELETE'],
+		credentials: true,
+	}),
+);
 
 const resultRoutes = require('./routes/result-route');
 app.use('/api/chatgpt', resultRoutes);
@@ -11,4 +23,4 @@ app.use('/api/chatgpt', resultRoutes);
 const bingRoutes = require('./routes/bing-route');
 app.use('/api/bing', bingRoutes);
 
-app.listen(3000);
+app.listen(5001);
