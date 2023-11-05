cb9343696c0d1a7e5dc1fa1d0e2f1c04eb2fc4de
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2023-11-04 19:55:17 -0500

    homepage

--- react-app-utd/src/App.js
+++ react-app-utd/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+function BasicButtonExample() {
+  return (
+    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    </DropdownButton>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='maincomponent'>
+
+        <h1>InsuranceHelper</h1>
+        <p>This web application will output the top natural disasters that happen in your state to help you get an idea of the insurance your small business may need</p>
+        <BasicButtonExample/>
+        <div>
+          <p> </p>
+        </div>
+      </div>
+      <div className="secondcomponent">
+        <h2 className='subheaders'>Customer Response Analysis</h2>
+      </div>
     </div>
   );
 }
 
+
 export default App;
+
