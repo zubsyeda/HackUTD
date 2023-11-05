e442a16d98139321c72e3a2e5975dfc02c0961b1
choimyungbin <bbin852456.bbc@gmail.com>
2023-11-05 04:15:53 -0600

    backend implementation

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.json());
 
 const resultRoutes = require('./routes/result-route');
 app.use('/api/chatgpt', resultRoutes);
@@ -10,4 +11,4 @@ app.use('/api/chatgpt', resultRoutes);
 const bingRoutes = require('./routes/bing-route');
 app.use('/api/bing', bingRoutes);
 
-app.listen(6000);
+app.listen(3000);
