6656a4842e8f435acb4f62134bdbcddda1af38d4
choimyungbin <bbin852456.bbc@gmail.com>
2023-11-04 19:31:14 -0500

    adapted ChatGPT api

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 const resultRoutes = require('./routes/result-route');
-app.use('/', resultRoutes);
+app.use('/api/chatgpt', resultRoutes);
 
 app.listen(6000);
--- backend/routes/result-route.js
+++ backend/routes/result-route.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const openai = require('openai');
+// API KEY
+const apiKey = process.env.OPENAI_API_KEY;
+const openaiClient = new openai({ key: apiKey });
 
-router.get('/', (req, res, next) => {
-	console.log('GET Request in results');
-	res.json({ message: 'test' });
+router.get('/:stateName', (req, res, next) => {
+	const stateName = req.params.stateName;
+	// queries for chatgpt
+	const query = `Tell me the weather in ${stateName}`;
+
+	// call ChatGPT API
+	openaiClient.chat.completions
+		.create({
+			model: 'gpt-3.5-turbo', // GPT-3.5 Davinci engine
+			messages: [
+				{ role: 'system', content: 'You are a helpful assistant.' },
+				{ role: 'user', content: query },
+			],
+		})
+		.then((response) => {
+			// extract text from api response
+			const result = response.choices[0].message.content;
+			console.log(result); // ChatGPT response text
+			res.json({ result });
+		})
+		.catch((error) => {
+			console.error('API req error:', error);
+		});
 });
 
 module.exports = router;
